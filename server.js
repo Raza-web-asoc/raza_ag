f63c62d050935d05c0577798ab2492e6653890b9
Juan Ovalle <jpovalleg@gmail.com>
2025-03-01 00:31:01 -0500

    Added prometheus and 'front-metrics' to receive metrics from Frontend

--- server.js
+++ server.js
@@ -1,41 +1,60 @@
-import { ApolloServer } from 'apollo-server-express';
-import { graphqlUploadExpress } from 'graphql-upload';
-import express from 'express';
-import prometheusMiddleware from 'express-prometheus-middleware';
+import { ApolloServer } from "apollo-server-express";
+import { graphqlUploadExpress } from "graphql-upload";
+import express from "express";
+import prometheusMiddleware from "express-prometheus-middleware";
 
-import { typeDefs as userTypeDefs } from './schemas/userSchemas.js';
-import { resolvers as userResolvers } from './resolvers/userResolvers.js';
+import { typeDefs as userTypeDefs } from "./schemas/userSchemas.js";
+import { resolvers as userResolvers } from "./resolvers/userResolvers.js";
 
-import { typeDefs as petTypeDefs } from './schemas/petsSchemas.js';
-import { resolvers as petResolvers } from './resolvers/petsResolvers.js';
+import { typeDefs as petTypeDefs } from "./schemas/petsSchemas.js";
+import { resolvers as petResolvers } from "./resolvers/petsResolvers.js";
 
-import { typeDefs as imagesTypeDefs } from './schemas/imagesSchemas.js';
-import { resolvers as imagesResolvers } from './resolvers/imagesResolvers.js';
+import { typeDefs as imagesTypeDefs } from "./schemas/imagesSchemas.js";
+import { resolvers as imagesResolvers } from "./resolvers/imagesResolvers.js";
 
-import { typeDefs as speciesTypeDefs } from './schemas/speciesSchemas.js';
-import { resolvers as speciesResolvers } from './resolvers/speciesResolvers.js';
+import { typeDefs as speciesTypeDefs } from "./schemas/speciesSchemas.js";
+import { resolvers as speciesResolvers } from "./resolvers/speciesResolvers.js";
 
-import { typeDefs as breedsTypeDefs } from './schemas/breedsSchemas.js';
-import { resolvers as breedsResolvers } from './resolvers/breedsResolvers.js';
+import { typeDefs as breedsTypeDefs } from "./schemas/breedsSchemas.js";
+import { resolvers as breedsResolvers } from "./resolvers/breedsResolvers.js";
 
-import { typeDefs as matchTypeDefs } from './schemas/matchSchemas.js';
-import { resolvers as matchResolvers } from './resolvers/matchResolvers.js';
+import { typeDefs as matchTypeDefs } from "./schemas/matchSchemas.js";
+import { resolvers as matchResolvers } from "./resolvers/matchResolvers.js";
 
-import { typeDefs as chatTypeDefs } from './schemas/chatSchemas.js';
-import { resolvers as chatResolvers } from './resolvers/chatResolvers.js';
+import { typeDefs as chatTypeDefs } from "./schemas/chatSchemas.js";
+import { resolvers as chatResolvers } from "./resolvers/chatResolvers.js";
+
+import client from "prom-client"; // Cliente Prometheus
+import cors from "cors";
 
 async function startServer() {
   const app = express();
+  app.use(cors());
+  app.use(express.json());
 
   // Configurar la carga de archivos (graphql-upload)
   app.use(graphqlUploadExpress());
 
-  // Configurar Prometheus para métricas
-  app.use(prometheusMiddleware({
-    metricsPath: '/metrics',
-    collectDefaultMetrics: true,
-    requestDurationBuckets: [0.1, 0.5, 1, 3, 5]
-  }));
+  app.use(
+    prometheusMiddleware({
+      metricsPath: "/metrics",
+      collectDefaultMetrics: true,
+      labels: { source: "backend" },
+    })
+  );
+
+  const frontendRequestCounter = new client.Counter({
+    name: "frontend_http_requests_total",
+    help: "Total de solicitudes HTTP recibidas desde el frontend",
+    labelNames: ["method", "status", "url"],
+  });
+
+  // Endpoint para recibir métricas desde el frontend
+  app.get('/front-metrics', (req, res) => {
+    const { method, status, url } = req.query;
+    frontendRequestCounter.inc({ method, status, url }); // Incrementar métrica
+    res.json({ msg: 'Metricas actualizadas' })
+  });
 
   const server = new ApolloServer({
     typeDefs: [
@@ -45,7 +64,8 @@ async function startServer() {
       speciesTypeDefs,
       breedsTypeDefs,
       matchTypeDefs,
-      chatTypeDefs],
+      chatTypeDefs,
+    ],
     resolvers: [
       userResolvers,
       petResolvers,
@@ -53,7 +73,8 @@ async function startServer() {
       speciesResolvers,
       breedsResolvers,
       matchResolvers,
-      chatResolvers],
+      chatResolvers,
+    ],
   });
 
   await server.start();
@@ -62,8 +83,8 @@ async function startServer() {
   const PORT = process.env.PORT || 4000;
 
   app.listen(PORT, () => {
-    console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`);
-    console.log(`📊 Metrics available at http://localhost:${PORT}/metrics`);
+    console.log(`Server ready at http://localhost:${PORT}${server.graphqlPath}`);
+    console.log(`Metrics available at http://localhost:${PORT}/metrics`);
   });
 }
 
